7f70ebf5aecd81b3b43e5ce1904c5b592d684bd2
boesiii <boesiii@yahoo.com>
2016-06-23 09:41:26 -0400

    version 0.4

--- flowtrace.py
+++ flowtrace.py
@@ -71,92 +71,112 @@ class flowTrace:
 
     # run method that performs all the real work
     def run(self):
-		QMessageBox.information(None, "Flow Trace", "Press Ok to start Flow Trace")
-		#setup final selection list
-		final_list = []
-		#setup temporary selection list
-		selection_list = []
-		#add tolerance value
-		tolerance = 1
-		#get current layer
-		clayer = self.iface.mapCanvas().currentLayer()
-		
-		if clayer is None:
-			return 
-		
-		#get provider
-		provider = clayer.dataProvider()
-		
-		#get selected features
-		features = clayer.selectedFeatures()
-		
-		#if not clayer.selectedFeatures():
-		#	QMessageBox.information(None, "Flow Trace", "No Features Selected")
-		#	exit(0)
-			
-		
-		#iterate thru features to add to lists
-		for feature in features:			
-			# add selected features to final list
-			final_list.append(feature.id())
-			# add selected features to selection list for while loop
-			selection_list.append(feature.id())
-		
-		
-		
-		#loop thru selection list
-		while selection_list:
-			#get selected features
-			request = QgsFeatureRequest().setFilterFid(selection_list[0])
-			feature = clayer.getFeatures(request).next()			
-			
-			# get list of nodes
-			nodes = feature.geometry().asPolyline()
-			
-			# get upstream node
-			upstream_coord = nodes[0]
-						
-			# select all features around upstream coordinate using a bounding box
-			rectangle = QgsRectangle(upstream_coord.x() - 5, upstream_coord.y() - 5, upstream_coord.x() + 5, upstream_coord.y() + 5)
-			request = QgsFeatureRequest().setFilterRect(rectangle)
-			features = clayer.getFeatures(request)
-						
-			#iterate thru requested features
-			for feature in features:
-			
-				#get list of nodes
-				nodes = feature.geometry().asPolyline()
-				
-				#get downstream node
-				downstream_coord = nodes[-1]
-				
-				#setup distance
-				distance = QgsDistanceArea()
-				
-				#get distance from downstream node to upstream node
-				dist = distance.measureLine(downstream_coord, upstream_coord)
-				
-				#Below is the distance rounded to 2 decimal places only needed during testing
-				#dist = round (distance.measureLine(downstream_coord, upstream_coord), 2)
-				
-				if dist < tolerance:
-					#add feature to final list
-					final_list.append(feature.id())
-					
-					#add feature to selection list to keep selecting upstream line segments
-					#selection_list.append(feature.id())
-										
-					if feature.id() not in selection_list:
-						#add feature to selection list
-						selection_list.append(feature.id())
-				
-			#remove feature from selection list
-			selection_list.pop(0)
-			
-			
-		#select features using final_list			
-		clayer.setSelectedFeatures(final_list)
-		
-		#refresh the canvas
-		self.iface.mapCanvas().refresh()
-		QMessageBox.information(None, "Flow Trace Complete", "Total Features Selected: " + str(len(final_list)))
+        QMessageBox.information(None, "Flow Trace", "Press Ok to start Flow Trace")
+        #setup final selection list
+        final_list = []
+        #setup temporary selection list
+        selection_list = []
+        #add tolerance value
+        tolerance = 1
+        #get current layer
+        clayer = self.iface.mapCanvas().currentLayer()
+        
+        if clayer is None:
+            return 
+        
+        #get provider
+        provider = clayer.dataProvider()
+        
+        #get selected features
+        features = clayer.selectedFeatures()
+        
+        #if not clayer.selectedFeatures():
+        #   QMessageBox.information(None, "Flow Trace", "No Features Selected")
+        #   exit(0)
+        
+        # get crs for tolerance setting
+        crs = self.iface.activeLayer().crs().authid()
+        print crs
+        if crs == 'EPSG:4269':
+            rec = .0001
+            tolerance = .0001
+        else:
+            rec = 1
+        #print tolerance
+        
+            
+        
+        #iterate thru features to add to lists
+        for feature in features:            
+            # add selected features to final list
+            final_list.append(feature.id())
+            # add selected features to selection list for while loop
+            selection_list.append(feature.id())
+            #get feature geometry
+            geom = feature.geometry()
+            if geom.type() <> QGis.Line:
+                print "Geometry not allowed"
+                QMessageBox.information(None, "Flow Trace", "Geometry not allowed, \nPlease select line geometry only.")
+                return
+            
+        
+        
+        
+        #loop thru selection list
+        while selection_list:
+        
+            #get selected features
+            request = QgsFeatureRequest().setFilterFid(selection_list[0])
+            feature = clayer.getFeatures(request).next()            
+            
+            # get list of nodes
+            nodes = feature.geometry().asPolyline()
+            
+            # get upstream node
+            upstream_coord = nodes[0]
+                        
+            # select all features around upstream coordinate using a bounding box
+            rectangle = QgsRectangle(upstream_coord.x() - rec, upstream_coord.y() - rec, upstream_coord.x() + rec, upstream_coord.y() + rec)
+            request = QgsFeatureRequest().setFilterRect(rectangle)
+            features = clayer.getFeatures(request)
+                        
+            #iterate thru requested features
+            for feature in features:
+            
+                #get list of nodes
+                #print feature.id()
+                nodes = feature.geometry().asPolyline()
+                
+                #get downstream node
+                downstream_coord = nodes[-1]
+                
+                #setup distance
+                distance = QgsDistanceArea()
+                
+                #get distance from downstream node to upstream node
+                dist = distance.measureLine(downstream_coord, upstream_coord)
+                
+                #Below is the distance rounded to 2 decimal places only needed during testing
+                #dist = round (distance.measureLine(downstream_coord, upstream_coord), 2)
+                
+                if dist < tolerance:
+                    #add feature to final list
+                    final_list.append(feature.id())
+                    
+                    #add feature to selection list to keep selecting upstream line segments
+                    #selection_list.append(feature.id())
+                                        
+                    if feature.id() not in selection_list:
+                        #add feature to selection list
+                        selection_list.append(feature.id())
+                
+            #remove feature from selection list
+            selection_list.pop(0)
+            
+            
+        #select features using final_list           
+        clayer.setSelectedFeatures(final_list)
+        
+        #refresh the canvas
+        self.iface.mapCanvas().refresh()
+        QMessageBox.information(None, "Flow Trace Complete", "Total Features Selected: " + str(len(final_list)))
